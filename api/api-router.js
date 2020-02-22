2d902948b8cc1c309f3ddb6036ba0c0296e3abf5
Katrina_Dierking <kalynndier@gmail.com>
2020-02-22 13:32:41 -0500

    api-router created

---
+++ api/api-router.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+const authRouter = require('../auth/auth-router');
+const usersRouter = require ('../users/users-router');
+
+router.use('/auth', authRouter);
+router.use('/users', usersRouter);
+
+router.get('/', (req, res) => {
+    res.json 
+    ({
+        api: "Let's get this party started"
+    });
+});
+
+module.exports = router;
