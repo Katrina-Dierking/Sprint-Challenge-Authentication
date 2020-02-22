2c69a01388553f1b1c4b016fb36a50710a95378e
Katrina_Dierking <kalynndier@gmail.com>
2020-02-22 12:22:40 -0500

    npm installed

--- api/server.js
+++ api/server.js
@@ -15,4 +15,8 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
+server.get('/', (req, res) => {
+    res.send("Is this thing on???")
+});
+
 module.exports = server;
